946e1f3ce97402df85e1b836300205439c832246
Bogdan Polak <bogdanpol@gmail.com>
2020-10-26 23:12:21 +0100

    Added two simple integration tests

--- test/Test.DiscountCalculator.pas
+++ test/Test.DiscountCalculator.pas
@@ -3,36 +3,58 @@ unit Test.DiscountCalculator;
 interface
 
 uses
-  DUnitX.TestFramework;
+  DUnitX.TestFramework, Database.Module;
+
+{$M+}
 
 type
+
   [TestFixture]
   TTestDiscountCalculator = class
+  private
   public
     [Setup]
     procedure Setup;
     [TearDown]
     procedure TearDown;
   published
-    procedure Test1;
+    procedure GetCustomerLevel_ShouldEqualStandard;
+    procedure UpdateOrderDiscount_Granted4;
   end;
 
 implementation
 
 procedure TTestDiscountCalculator.Setup;
 begin
+  DataModule1.FDConnection1.Open();
 end;
 
 procedure TTestDiscountCalculator.TearDown;
 begin
 end;
 
-procedure TTestDiscountCalculator.Test1;
+procedure TTestDiscountCalculator.GetCustomerLevel_ShouldEqualStandard;
+var
+  actual: string;
 begin
-  Assert.Fail();
+  actual := DataModule1.GetCustomerLevel('DE136695976');
+  Assert.AreEqual('standard', actual);
+end;
+
+procedure TTestDiscountCalculator.UpdateOrderDiscount_Granted4;
+var
+  orderid: Integer;
+  actual: Integer;
+begin
+  orderid := 5;
+  DataModule1.UpdateOrderDiscount(orderid, 4);
+  actual := DataModule1.FDConnection1.ExecSQLScalar
+    ('select GrantedDiscount from Orders where OrderId = :OrderID', [orderid]);
+  Assert.AreEqual(4, actual);
 end;
 
 initialization
-  TDUnitX.RegisterTestFixture(TTestDiscountCalculator);
+
+TDUnitX.RegisterTestFixture(TTestDiscountCalculator);
 
 end.
